9a3961d7501a327eee2e6bd0e886739ff77a08c8
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-13 09:17:22 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
   if array.length == 1
-  array.join
-
-  array.join(" and ")
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif
+    array.join(" and ")
 end
-
-def oxford_comma(array)
-  array.join(" and ")
+end
 end
