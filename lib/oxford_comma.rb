ea3603f174bfbb5541485197707816d197bd8e5c
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-13 09:34:10 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,8 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2
     array.join(" and ")
-  else array.length > 3
-    array[-2] << " and "
+  else array.length >= 3
     array.join(", ")
+    array[-2] << " and "
 end
 end
